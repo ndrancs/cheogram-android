265919bb79758cdff5b395bb8a29030fe0785392
Stephen Paul Weber <singpolyma@singpolyma.net>
2024-08-14 11:21:47 -0500

    Implement experimenal WebXDC "realtime" API

--- src/cheogram/java/com/cheogram/android/WebxdcPage.java
+++ src/cheogram/java/com/cheogram/android/WebxdcPage.java
@@ -18,6 +18,7 @@ import android.view.LayoutInflater;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Base64;
 import android.webkit.JavascriptInterface;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -147,6 +148,13 @@ public class WebxdcPage implements ConversationPage {
 
 	public String getNode() {
 		return "webxdc\0" + source.getUuid();
+	}
+
+	public boolean threadMatches(final Element thread) {
+		if (thread == null) return false;
+		if (thread.getContent() == null) return false;
+		if (source.getThread() == null) return false;
+		return thread.getContent().equals(source.getThread().getContent());
 	}
 
 	public boolean openUri(Uri uri) {
@@ -324,6 +332,7 @@ public class WebxdcPage implements ConversationPage {
 				}
 				ShortcutManagerCompat.requestPinShortcut(xmppConnectionService, builder.build(), null);
 			} else {
+				binding.webview.loadUrl("about:blank");
 				remover.accept(WebxdcPage.this);
 			}
 		});
@@ -340,6 +349,10 @@ public class WebxdcPage implements ConversationPage {
 		binding.webview.post(() -> binding.webview.loadUrl("javascript:__webxdcUpdate();"));
 	}
 
+	public void realtimeData(String base64) {
+		binding.webview.post(() -> binding.webview.loadUrl("javascript:__webxdcRealtimeData('" + base64.replace("'", "").replace("\\", "").replace("+", "%2B") + "');"));
+	}
+
 	protected Jid selfJid() {
 		final Conversation conversation = (Conversation) source.getConversation();
 		if (conversation.getMode() == Conversation.MODE_MULTI && !conversation.getMucOptions().nonanonymous()) {
@@ -352,6 +365,11 @@ public class WebxdcPage implements ConversationPage {
 	protected class InternalJSApi {
 		@JavascriptInterface
 		public String selfAddr() {
+			final Conversation conversation = (Conversation) source.getConversation();
+			if (conversation.getMode() == Conversation.MODE_MULTI && !conversation.getMucOptions().nonanonymous()) {
+				final var occupantId = conversation.getMucOptions().getSelf().getOccupantId();
+				if (occupantId != null) return occupantId;
+			}
 			return "xmpp:" + Uri.encode(selfJid().toEscapedString(), "@/+");
 		}
 
@@ -460,6 +478,21 @@ public class WebxdcPage implements ConversationPage {
 				e.printStackTrace();
 				return e.toString();
 			}
+		}
+
+		@JavascriptInterface
+		public void sendRealtime(byte[] data) {
+			Message message = new Message(source.getConversation(), null, Message.ENCRYPTION_NONE);
+			message.addPayload(new Element("no-store", "urn:xmpp:hints"));
+			Element webxdc = new Element("x", "urn:xmpp:webxdc:0");
+			message.addPayload(webxdc);
+			webxdc.addChild("data").setContent(Base64.encodeToString(data, Base64.NO_WRAP));
+			message.setThread(source.getThread());
+			if (source.isPrivateMessage()) {
+				Message.configurePrivateMessage(message, source.getCounterpart());
+			}
+			message.setBody((String) null);
+			xmppConnectionService.sendMessage(message);
 		}
 	}
 }
